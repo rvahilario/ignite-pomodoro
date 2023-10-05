eb81da50f42d4c9da7c2da2bf5134fbc3028a4cd
rvahilario <rvahilario@gmail.com>
2023-10-04 22:21:20 -0300

    add: create Header and navigation between pages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,58 @@
+import styled from 'styled-components'
+import igniteLogo from '../assets/ignite-logo.svg'
+import { NavLink } from 'react-router-dom'
+import { Scroll, Timer } from '@phosphor-icons/react'
+
 type Props = {}
 
 export function Header({}: Props) {
-  return <div>Header</div>
+  return (
+    <HeaderContainer>
+      <img src={igniteLogo} />
+
+      <StyledNav>
+        <NavLink to={'/'} title="Timer">
+          <Timer size={'1.5rem'} />
+        </NavLink>
+        <NavLink to={'/history'} title="Historic">
+          <Scroll size={'1.5rem'} />
+        </NavLink>
+      </StyledNav>
+    </HeaderContainer>
+  )
 }
+
+const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 2.5rem;
+  margin-top: 3rem;
+`
+
+const StyledNav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  gap: 0.5rem;
+
+  a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 3rem;
+    height: 3rem;
+
+    border-top: 3px solid transparent;
+    border-bottom: 3px solid transparent;
+    color: ${({ theme }) => theme['gray-100']};
+
+    &:hover {
+      border-bottom: 3px solid ${({ theme }) => theme['green-500']};
+    }
+
+    &.active {
+      color: ${({ theme }) => theme['green-500']};
+    }
+  }
+`
--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import { Header } from '../components/Header'
+import styled from 'styled-components'
 import { Outlet } from 'react-router-dom'
+import { Header } from '../components/Header'
 
 type Props = {}
 
 export function DefaultLayout({}: Props) {
   return (
-    <>
+    <Container>
       <Header />
       <Outlet />
-    </>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: calc(100vh - 10rem);
+  width: 70rem;
+  margin: 5rem auto;
+  padding: 0 2.5rem 2.5rem;
+  background: ${({ theme }) => theme['gray-800']};
+`
